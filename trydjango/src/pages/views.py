05fb95aeb6ae79d7f3c05918684cb7e97c2fb17b
fishow36 <fishow36@gmail.com>
2019-06-04 19:55:56 +0300

    added recent words

--- trydjango/src/pages/views.py
+++ trydjango/src/pages/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from words.models import Word
 # Create your views here.
 def home_view(request, *args, **kwargs):
-	return render(request, 'home.html', {})
+	#хочу вывести три последних слова пока
+	latest_id = Word.objects.latest('id').id
+	home_context = {
+	'word1' : Word.objects.get(id=latest_id),
+	'word2' : Word.objects.get(id=latest_id-1),
+	'word3' : Word.objects.get(id=latest_id-2)
+	}
+	return render(request, 'home.html', home_context)
 
 def contacts(request, *args, **kwargs):
 	about_context = {
